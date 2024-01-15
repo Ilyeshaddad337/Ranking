b65b7bd96c8f55920bfa058651c807998e217655
Ilyeshaddad337 <i.haddad@esi-sba.dz>
2024-01-15 21:51:30 +0100

    deleted kouadri soumia

--- Classement.js
+++ Classement.js
@@ -4,7 +4,7 @@ let startt = 3
 const input = document.querySelector('#searching');
 const select = document.querySelector('select');
 
-const excluded = ['BELARBIChifae ribal', 'BERRAHEL', 'BELARBI Chifae']
+const excluded = ['BELARBIChifae ribal', 'BERRAHEL', 'KOUADRISOUMIA']
 var current_promo = 'promo-2021'
 //"./2022-2023/sfsd-s1-emd1-promo.csv","./2022-2023/algebre3-s1-emd1-promo.csv","./2022-2023/analyse3-s1-emd1-promo.csv","./2022-2023/proba-s1-emd1-promo.csv","./2022-2023/eln2-s1-emd1-promo.csv","./2022-2023/eng-s1-emd1-promo.csv","./2022-2023/archi2-s1-emd1-promo.csv",
 //  './promo-2021/2022-2023/archi2-s1-total-promo.csv', './promo-2021/2022-2023/proba-s1-emd2-promo.csv','./promo-2021/2022-2023/algebre3-s1-emd2-promo.csv','./promo-2021/2022-2023/sfsd-s1-emd2-promo.csv', './promo-2021/2022-2023/economie-s1-emd-promo.csv','./promo-2021/2022-2023/eln2-s1-total.csv','./promo-2021/2022-2023/eng-s1-emd2-promo.csv',
